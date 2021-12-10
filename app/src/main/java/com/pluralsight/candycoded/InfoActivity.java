59cf3c79c23fb8c0c0c382db2f29750ce2d7a437
Amr Salah <amrhady3@gmail.com>
2021-12-10 10:31:45 +0200

    Add intent to open a dialer with store phone number

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -26,6 +26,9 @@ public class InfoActivity extends AppCompatActivity {
 
     }
 
+    // ***
+    // TODO - Task 2 - Launch the Google Maps Activity
+    // ***
     public void createMapIntent(View view) {
         Uri locationUri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, locationUri);
@@ -37,11 +40,13 @@ public class InfoActivity extends AppCompatActivity {
     }
 
     // ***
-    // TODO - Task 2 - Launch the Google Maps Activity
-    // ***
-
-    // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    public void createPhoneIntent(View view) {
+        Uri phoneUri = Uri.parse("tel:0123456789");
+        Intent dialIntent = new Intent(Intent.ACTION_DIAL);
+        dialIntent.setData(phoneUri);
+        startActivity(dialIntent);
+    }
 
 }
